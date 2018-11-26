4b456501e9e4506caaaa5e8e6a1c46e4cea94439
Vanik <vaniksarajyan@gmail.com>
2018-11-27 00:16:01 +0400

    add validations

--- library-api/app/models/author.rb
+++ library-api/app/models/author.rb
@@ -1,3 +1,6 @@
 class Author < ApplicationRecord
     has_many :books
+
+    validates :full_name, length: { in: 2..30 }
+    validates :email, uniqueness: {message: "This email has been taken"}, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
--- library-api/app/models/book.rb
+++ library-api/app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :author
+
+  validates :title, length: {in: 1..40}
+  validates :price, :inclusion => {:in => 0..200}
+  validates :author_id, presence: true
 end
